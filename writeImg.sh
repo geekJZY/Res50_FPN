52795f058824d1fbf1a06674682ea9ce5bdb19f9
geekJZY <jiangziyu@tamu.edu>
2018-09-25 23:29:44 -0500

    writeImg.sh

---
+++ writeImg.sh
@@ -0,0 +1,12 @@
+python writeImg.py --epoch "7500"
+zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/res50_lovzas_focal_7500.zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/*.png
+python writeImg.py --epoch "10000"
+zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/res50_lovzas_focal_10000.zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/*.png
+python writeImg.py --epoch "12500"
+zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/res50_lovzas_focal_12500.zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/*.png
+python writeImg.py --epoch "15000"
+zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/res50_lovzas_focal_15000.zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/*.png
+python writeImg.py --epoch "17500"
+zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/res50_lovzas_focal_17500.zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/*.png
+python writeImg.py --epoch "20000"
+zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/res50_lovzas_focal_20000.zip /mnt/sdb/jiangziyu/data/offical_crossvali/Label/*.png
